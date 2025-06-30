cbe6b35c3c49ce2333bfc0693335c5b026a8ea7b
roumak248 <roumakmaity@gmail.com>
2025-06-30 13:08:02 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,9 @@
+n=int(input("Enter a number:  "))
+
+def factorial(n):
+    if n<2:
+        return 1
+    else:
+        return n*(factorial(n-1))
+result=factorial(n)
+print("The factorial of ",n," is",result)
---
+++ square root,sine,log.py
@@ -0,0 +1,16 @@
+n=int(input("Enter a number: "))
+#square root
+def squareroot(n):
+    return n**(1/2)
+result=squareroot(n)
+print("square root: ",result)
+
+#logarithm
+import math
+l=math.log(n)
+print("logarithm: ",l)
+
+#value of sine
+import math
+s=math.sin(n)
+print("sine: ",s)
